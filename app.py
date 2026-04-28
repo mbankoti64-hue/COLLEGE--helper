6a021aceb5ad7a100d01fecb1ca3c5a06020a839
mbankoti64-hue <mbankoti64@gmail.com>
2026-04-28 21:54:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,17 +45,36 @@ section[data-testid="stSidebar"] {
 # Header (Clean & Working ✅)
 # -------------------------------
 
-st.image("logo.jpg.jpeg", width=200)
+# st.image("logo.jpg.jpeg", width=200)
 
-st.markdown("""
-<h1 style='text-align:center; color:#22c55e; margin-top:-20px;'>
-Graphic Era Smart Assistant
-</h1>
+# st.markdown("""
+# <h1 style='text-align:center; color:#22c55e; margin-top:-20px;'>
+# Graphic Era Smart Assistant
+# </h1>
 
-<p style='text-align:center; color:#cbd5f5;'>
-Your 24/7 College Help Partner 🤖
-</p>
-""", unsafe_allow_html=True)
+# <p style='text-align:center; color:#cbd5f5;'>
+# Your 24/7 College Help Partner 🤖
+# </p>
+# """, unsafe_allow_html=True)
+# -------------------------------
+# Header (Logo + Title Same Line 🔥)
+# -------------------------------
+
+col1, col2 = st.columns([1, 3])
+
+with col1:
+    st.image("logo.jpg.jpeg", width=120)
+
+with col2:
+    st.markdown("""
+    <h1 style='color:#22c55e; margin-top:20px;'>
+    Graphic Era Smart Assistant
+    </h1>
+    
+    <p style='color:#cbd5f5; margin-top:-10px;'>
+    Your 24/7 College Help Partner 🤖
+    </p>
+    """, unsafe_allow_html=True)
 
 # -------------- -----------------
 # Welcome Message (only once)
